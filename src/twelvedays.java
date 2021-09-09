84774a4194c2195cc1b0fac11fb86f503c6cb04b
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-09 10:19:42 -0700

    Initial commit

---
+++ src/twelvedays.java
@@ -0,0 +1,130 @@
+public class twelvedays {
+    public static void d1() {
+        System.out.println("On the first day of Christmas, my true love sent to me\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d2() {
+        System.out.println("On the second day of Christmas, my true love sent to me\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d3() {
+        System.out.println("On the third day of Christmas, my true love sent to me\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d4() {
+        System.out.println("On the fourth day of Christmas, my true love sent to me\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d5() {
+        System.out.println("On the fifth day of Christmas, my true love sent to me\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear treee\n");
+    }
+    public static void d6() {
+        System.out.println("On the sixth day of Christmas, my true love sent to me\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d7() {
+        System.out.println("On the seventh day of Christmas, my true love sent to me\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d8() {
+        System.out.println("On the eighth day of Christmas, my true love sent to me\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d9() {
+        System.out.println("On the ninth day of Christmas, my true love sent to me\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d10() {
+        System.out.println("On the tenth day of Christmas, my true love sent to me\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d11() {
+        System.out.println("On the eleventh day of Christmas, my true love sent to me\n" +
+                "Eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void d12() {
+        System.out.println("On the twelfth day of Christmas, my true love sent to me\n" +
+                "Twelve drummers drumming\n" +
+                "Eleven pipers piping\n" +
+                "Ten lords a-leaping\n" +
+                "Nine ladies dancing\n" +
+                "Eight maids a-milking\n" +
+                "Seven swans a-swimming\n" +
+                "Six geese a-laying\n" +
+                "Five golden rings\n" +
+                "Four calling birds\n" +
+                "Three french hens\n" +
+                "Two turtle doves, and\n" +
+                "A partridge in a pear tree\n");
+    }
+    public static void main(String[] args) {
+        d1();
+        d2();
+        d3();
+        d4();
+        d5();
+        d6();
+        d7();
+        d8();
+        d9();
+        d10();
+        d11();
+        d12();
+    }
+}
